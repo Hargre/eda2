921b08f5d637c71aaceee0bde6be24fc8e4ca64b
Felipe Hargreaves <fhargreaves00@gmail.com>
2017-05-19 22:41:27 -0300

    Add question 04

---
+++ Lista 4/q4/lista04_questao04.py
@@ -0,0 +1,43 @@
+def gen_sequence():
+    yahoo = "buy Yahoo"
+    ebay = "buy Ebay"
+    oracle = "buy Oracle"
+    amazon = "buy Amazon"
+
+    sequence = [amazon, yahoo, ebay, yahoo, yahoo, oracle]
+
+    return sequence
+
+def gen_subsequence():
+    yahoo = "buy Yahoo"
+    ebay = "buy Ebay"
+    oracle = "buy Oracle"
+    amazon = "buy Amazon"
+
+    subsequence = [amazon, yahoo, ebay, yahoo, oracle]
+
+    return subsequence
+
+def is_subsequence(sequence, subsequence):
+    """ Given two sequences, returns whether the second is a subsequence of the first. """
+    events_found = 0
+    for event in sequence:
+        if event == subsequence[events_found]:
+            events_found += 1
+        if events_found == len(subsequence):
+            return True
+    return False
+
+def main():
+    sequence = gen_sequence()
+    subsequence = gen_subsequence()
+
+    if is_subsequence(sequence, subsequence):
+        print("S' is a subsquence of S")
+    else:
+        print("Not subsequence")
+
+if __name__ == "__main__":
+    main()
+
+
